ffc8080fcfa3d7e199d17ab364ae6899e4e9f066
Jose Toledo <toledojose60@gmail.com>
2021-10-18 22:36:41 -0700

    forgot to import LHD_optim module

--- pyLHD/__init__.py
+++ pyLHD/__init__.py
@@ -3,4 +3,4 @@ from pyLHD.utils import *
 from pyLHD.base_designs import *
 from pyLHD.OLHD import *
 from pyLHD.misc import *
-from pyLHD.simulated_annealing_LHD import SA
+from pyLHD.LHD_optim import *
