6bc2b26eddffb75741eae742a4b9690444387ae4
Jose Toledo <toledojose60@gmail.com>
2024-02-07 08:10:36 -0800

    added UPC_lower_bound

--- pyLHD/projections.py
+++ pyLHD/projections.py
@@ -1,6 +1,44 @@
 import numpy as np
-from pyLHD.helpers import is_prime, WilliamsTransform,level_permutation
+from pyLHD.helpers import is_prime, WilliamsTransform, level_permutation, is_balanced_design
 from pyLHD.base import GoodLatticePoint
+import numpy.typing as npt
+
+def UPC_lower_bound(arr: npt.ArrayLike) -> float:
+  """Uniform Projection Criteria Lower Bound
+
+  Args:
+      arr (npt.ArrayLike): A numpy ndarray
+
+  Returns:
+      The lower bound of the Uniform projection criteria, whenever `arr` is a balanced $(N,s^m)$ design
+  
+  Notes:
+      The lower bound is achieved if and only if the design $D$ is an equidistant design under the $L_1$-distance
+  
+  Example:
+  ```{python}
+  import pyLHD
+  sample = pyLHD.EquidistantLHD(N=11)
+  sample
+  ```
+  ```{python}
+  pyLHD.UniformProCriterion(sample)
+  ```
+  ```{python}
+  pyLHD.UPC_lower_bound(sample)
+  ```
+  """
+  try:
+    s = np.unique(arr).size
+    is_balanced_design(arr, s = s)
+  except ValueError as e:
+    print(f"Error: {e}")
+  n,m = arr.shape
+  val1 = 5*m*(4*(s**4) + 2*(13*n -17)*(s**2) - n +5) - (n-1)*(8*(s**4)+150*(s**2)-33)
+  val2 = 720*(m-1)*(n-1)*(s**4)
+  val3 = (1+ ((-1)**s))/(64*(s**4))
+  return (val1/val2) + val3
+
 
 def best_linear_permutation(N:int) -> int:
   """Optimal linear permutation value to minimize the uniform projection criterion
